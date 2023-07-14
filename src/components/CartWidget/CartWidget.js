f74783fb19ec0d7150c97de85841c47a17454309
Andrea <calacheandrea@gmail.com>
2023-07-14 15:44:22 -0300

    estilos

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,8 +2,9 @@ import carrito from './assets/carrito.png'
 
 const CartWidget = () => {
     return (
+
          <div>
-            <img src={carrito} alt="cart-widget"/>
+            <img class="image is-48x48" src={carrito} alt="cart-widget"/>
             0
          </div>
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,22 @@
 import CartWidget from "../CartWidget/CartWidget"
+import { SiArduino } from 'react-icons/si';
 
 const NavBar = () => {
     return (
-        <nav>
-            <h3>Ecommerce</h3>
-            <div>
-                <button>Celulares</button>
-                <button>Tablets</button>
-                <button>Notebooks</button>
-            </div>
-            <CartWidget/>
+        <nav className="level">
+            <div className="container is-max-desktop">
+              <div className="notification is-primary">
+               <h2 className="title is-2"><SiArduino/> Ecommerce </h2>
+                
+                 <div class="buttons is-centered" >
+                    <button className="button is-black">Placas</button>
+                    <button className="button is-black">Sensores</button>
+                    <button className="button is-black">Actuadores</button>
+                 </div>
+                 <CartWidget/>
+                </div>
+           </div>
+            
         </nav>
     )
 }
