a4fced874e166211df839c5e541d7554a70690a4
Andrea <calacheandrea@gmail.com>
2023-07-14 15:47:12 -0300

    estilos

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
 const ItemListContainer = ({ greeting }) => {
     return (
         <div>
-            <h1>{greeting}</h1>
+            <h1 className="title is-1">{greeting}</h1>
         </div>
     )
 
