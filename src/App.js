c98dca11d170a33444b0b80244162c03ee8aa1fb
Andrea <calacheandrea@gmail.com>
2023-07-19 21:07:12 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
+import ProfileCard from "./components/ProfileCard";
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import AlexaImg from "./components/img/alexa.png";
+import CortanaImg from "./components/img/cortana.png";
+import SiriImg from "./components/img/siri.png";
 import "bulma/css/bulma.css";
 
 function App() {
@@ -9,8 +13,30 @@ function App() {
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenidos a nuestra tienda Arduino'}/>
+
+      <div>   
+    <div className="container">
+      <div className="section">
+        <div className="columns">
+          <div className="column is-4">
+            <ProfileCard titulo="Alexa" handle="@Alexa" img={AlexaImg} />
+          </div>
+          <div className="column is-4">
+            <ProfileCard titulo="Cortana" handle="@Cortana" img={CortanaImg}/>
+          </div>
+          <div className="column is-4">
+            <ProfileCard titulo="Siri" handle="@Siri" img={SiriImg}/>
+          </div>
+        </div>
+      </div>
+    </div>      
+  </div>
+      
     </div>
+   
+    
   );
+
 }
 
 export default App;
