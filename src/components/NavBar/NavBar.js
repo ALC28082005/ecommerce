f5891ff1f7eceaba53e5df77d0716d7a4a6f2f18
Andrea <calacheandrea@gmail.com>
2023-08-03 22:42:57 -0300

    actualizacion

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,11 +6,11 @@ import { NavLink, Link } from 'react-router-dom'
 const NavBar = () => {
     return (
 
-        <nav class="navbar">
+        <nav className="navbar is-primary" >
             <Link to ='/'>
            <h3><SiArduino/>Ecommerce</h3>
            </Link>
-            <div className='Categories'>
+            <div className="Categories">
                 <NavLink to={`/category/placas`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Placas</NavLink>
                 <NavLink to={`/category/sensores`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Sensores</NavLink>
                 <NavLink to={`/category/actuadores`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>Actuadores</NavLink>
