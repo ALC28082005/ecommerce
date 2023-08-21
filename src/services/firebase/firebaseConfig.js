a3a16732c17689dc8806260fb5a8caa38803dad0
Andrea <calacheandrea@gmail.com>
2023-08-20 23:43:59 -0300

    actualizacion

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestone} from 'firebase/firestone'
+import { getFirestone} from "firebase/firestone";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAslsol35C41oALnb6Nyrx-3rSUBSDn1rk",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId: "1:217667225030:web:eab48034c0cf511034dec7"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig); 
-export const db = getFirestone(app)
+
+export const db = getFirestone(app);
