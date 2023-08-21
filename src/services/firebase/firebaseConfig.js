398d0874e85d8f0c7655ae926efad3ea5e55478b
Andrea <calacheandrea@gmail.com>
2023-08-20 23:21:45 -0300

    actualizacion

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,17 +1,16 @@
-import { initializeApp} from "firebase/app"
+
+import { initializeApp } from "firebase/app";
 import { getFirestone} from 'firebase/firestone'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAslsol35C41oALnb6Nyrx-3rSUBSDn1rk",
-    authDomain: "calache-ecommerce.firebaseapp.com",
-    projectId: "calache-ecommerce",
-    storageBucket: "calache-ecommerce.appspot.com",
-    messagingSenderId: "217667225030",
-    appId: "1:217667225030:web:eab48034c0cf511034dec7"
-  }
+  apiKey: "AIzaSyAslsol35C41oALnb6Nyrx-3rSUBSDn1rk",
+  authDomain: "calache-ecommerce.firebaseapp.com",
+  projectId: "calache-ecommerce",
+  storageBucket: "calache-ecommerce.appspot.com",
+  messagingSenderId: "217667225030",
+  appId: "1:217667225030:web:eab48034c0cf511034dec7"
+};
 
-  const app =initializeApp(firebaseConfig);
-
-  export const db = getFirestone(app)
-
-  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
+export const db = getFirestone(app)
