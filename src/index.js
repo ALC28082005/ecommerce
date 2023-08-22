bedc7d4d3a55885b8576d04d367bf6cdd089932b
Andrea <calacheandrea@gmail.com>
2023-08-22 00:31:15 -0300

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import { StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { initializeApp } from 'firebase/app';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const firebaseConfig = {
+  apiKey: "AIzaSyAslsol35C41oALnb6Nyrx-3rSUBSDn1rk",
+  authDomain: "calache-ecommerce.firebaseapp.com",
+  projectId: "calache-ecommerce",
+  storageBucket: "calache-ecommerce.appspot.com",
+  messagingSenderId: "217667225030",
+  appId: "1:217667225030:web:eab48034c0cf511034dec7"
+};
+
+initializeApp(firebaseConfig);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
